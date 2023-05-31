a801b71e00fb82c379e94cb0c2bcff9abb85e0eb
H377F1RE <stulnikov.jaroslaw@gmail.com>
2023-05-31 15:35:35 +0300

    Add files

---
+++ TestProject/LessonTest.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class LessonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DateTime TestDate = DateTime.Now.Date;
+            Lesson L = new Lesson(TestDate, null, null, null, null, null, null);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Lesson L = new Lesson(null, null, null, null, null, null);
+        }
+    }
+}
